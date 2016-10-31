4d07823de28c4ad423628f9f90a92efde279a9aa
Paul M Furley <paul@paulfurley.com>
2016-10-31 13:48:02 +0000

    Add simple "stonewall" crypto configuration
    Using [ZeroMQ's Curve](http://curvezmq.org/) crypto.
    
    - subscribers require the publisher's public key to make a connection
    - subscriber-publisher socket is encrypted
    
    Note this is a *simple* config:
    
    - subscribers can connect from *any* IP address
    - subscribers do not have to authenticate at all - so anyone can connect
      to our publisher with the right protocol
    - subscribers *have* a public/private key, but that's only used for
      crypto, not for authentication
    - we're not using the concept of "domain" or "identity" at all

--- zpubsub/publisher/publisher.py
+++ zpubsub/publisher/publisher.py
@@ -1,37 +1,81 @@
 #!/usr/bin/env python
 
 
-import time
+import logging
+import os
 import random
+import time
 
 import zmq
+import zmq.auth
+from zmq.auth.thread import ThreadAuthenticator
+
 import utcdatetime
 
 from ..pb import price_update_pb2
+from ..utils import load_key_pair
 
 
 PORT = 5638
 TIME_TOPIC = b'time'
 RANDOM_TOPIC = b'random'
 PRICE_TOPIC = b'price'
+KEYS_DIR = os.path.join(os.path.dirname(__file__), 'keys')
 
 
 def main():
-    socket = make_socket()
+    logging.basicConfig(level=logging.DEBUG)
 
-    while True:
-        send_messages(socket)
-        time.sleep(5)
+    socket, authenticator = make_socket_and_authenticator()
+
+    assert authenticator.is_alive()
+
+    try:
+        while True:
+            send_messages(socket)
+            time.sleep(5)
+
+    except KeyboardInterrupt:
+        authenticator.stop()
+        raise
 
 
-def make_socket():
+def make_socket_and_authenticator():
     context = zmq.Context()
+
+    authenticator = make_authenticator(context)
+
     socket = context.socket(zmq.PUB)
+
+    configure_socket_keys(socket)
+
     socket.bind("tcp://*:{}".format(PORT))
-    return socket
+
+    return socket, authenticator
+
+
+def make_authenticator(context):
+    auth = ThreadAuthenticator(context)
+    auth.start()
+    # Note: by omitting auth.allow(ip) we're allowing connection from any IP
+
+    auth.configure_curve(
+        domain='*',
+        # CURVE_ALLOW_ANY: allow all client keys without checking
+        location=zmq.auth.CURVE_ALLOW_ANY
+    )
+    return auth
+
+
+def configure_socket_keys(s):
+    (s.curve_publickey, s.curve_secretkey) = load_key_pair(
+        KEYS_DIR, 'publisher', generate_on_fail=True
+    )
+    s.curve_server = True
 
 
 def send_messages(socket):
+    logging.debug("Sending messages to topics.")
     price_bytes = make_price_update_bytes()
     socket.send(PRICE_TOPIC + b' ' + price_bytes)
 
--- zpubsub/subscriber/subscriber.py
+++ zpubsub/subscriber/subscriber.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 
+import logging
 import sys
+import os
 
 import zmq
 
 from ..pb import price_update_pb2
+from ..utils import load_key_pair
 
 
+PUBLISHER_IP = '10.0.2.15'
 PORT = 5638
 TOPIC = b'price'
+KEYS_DIR = os.path.join(os.path.dirname(__file__), 'keys')
 
 
 def main():
-    context = zmq.Context()
-    socket = context.socket(zmq.SUB)
-    socket.connect("tcp://localhost:{}".format(PORT))
+    logging.basicConfig(level=logging.DEBUG)
 
-    socket.setsockopt(zmq.SUBSCRIBE, TOPIC)
+    socket = make_socket()
 
     while True:
         topic_and_data = socket.recv()
@@ -26,6 +29,44 @@ def main():
         price_update.ParseFromString(data)
 
         sys.stdout.write(repr(price_update) + '\n')
+
+
+def make_socket():
+    context = zmq.Context()
+
+    socket = context.socket(zmq.SUB)
+
+    configure_socket_keys(socket)
+    connect_and_subscribe_socket(socket)
+
+    return socket
+
+
+def connect_and_subscribe_socket(socket):
+    socket.connect("tcp://{ip}:{port}".format(ip=PUBLISHER_IP, port=PORT))
+    socket.setsockopt(zmq.SUBSCRIBE, TOPIC)
+
+
+def configure_socket_keys(s):
+    s.curve_publickey, s.curve_secretkey = load_key_pair(
+        KEYS_DIR, 'subscriber', generate_on_fail=True
+    )
+
+    s.curve_serverkey = load_publisher_public_key('publisher')
+
+
+def load_publisher_public_key(name):
+    publisher_public_file = os.path.join(KEYS_DIR, 'publisher.public.key')
+    if not os.path.isfile(publisher_public_file):
+        raise RuntimeError(
+            "Missing public key for publisher: I'm unable to connect and "
+            "authenticate the publisher. Copy {} to {} ".format(
+                os.path.basename(publisher_public_file),
+                os.path.abspath(KEYS_DIR))
+        )
+
+    publisher_public, _ = zmq.auth.load_certificate(publisher_public_file)
+    return publisher_public
 
 
 if __name__ == '__main__':
