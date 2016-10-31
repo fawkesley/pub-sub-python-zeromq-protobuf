5838f0676ae81c3fe3072e7a541dc63f92081c14
Paul M Furley <paul@paulfurley.com>
2016-10-31 11:07:56 +0000

    Factor out send_messages

--- zpubsub/publisher/publisher.py
+++ zpubsub/publisher/publisher.py
@@ -11,25 +11,16 @@ from ..pb import price_update_pb2
 
 
 PORT = 5638
+TIME_TOPIC = b'time'
+RANDOM_TOPIC = b'random'
+PRICE_TOPIC = b'price'
 
 
 def main():
     socket = make_socket()
 
-    time_topic = b'time'
-    random_topic = b'random'
-    price_topic = b'price'
-
     while True:
-        price_bytes = make_price_update_bytes()
-        socket.send(price_topic + b' ' + price_bytes)
-
-        time_bytes = str(utcdatetime.utcdatetime.now()).encode('utf-8')
-        socket.send(time_topic + b' ' + time_bytes)
-
-        random_bytes = str(random.random()).encode('utf-8')
-        socket.send(random_topic + b' ' + random_bytes)
-
+        send_messages(socket)
         time.sleep(5)
 
 
@@ -38,6 +29,17 @@ def make_socket():
     socket = context.socket(zmq.PUB)
     socket.bind("tcp://*:{}".format(PORT))
     return socket
+
+
+def send_messages(socket):
+    price_bytes = make_price_update_bytes()
+    socket.send(PRICE_TOPIC + b' ' + price_bytes)
+
+    time_bytes = str(utcdatetime.utcdatetime.now()).encode('utf-8')
+    socket.send(TIME_TOPIC + b' ' + time_bytes)
+
+    random_bytes = str(random.random()).encode('utf-8')
+    socket.send(RANDOM_TOPIC + b' ' + random_bytes)
 
 
 def make_price_update_bytes():
